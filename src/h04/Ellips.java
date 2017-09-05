c6f2bda6acadf7da6f2aa61b1562268be8ef3c3f
aaronvelt <aaronvelt000@gmail.com>
2017-09-05 21:04:17 +0200

    H4: Huis Vlag Diagram Ellips

---
+++ src/h04/Ellips.java
@@ -0,0 +1,18 @@
+package h04;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Ellips extends Applet {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        setBackground(Color.blue);
+        g.setColor(Color.yellow);
+        g.fillArc(100,100,150,75,90,365);
+    }
+}
