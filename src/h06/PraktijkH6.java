9cbadb851aaedc9a7c87ab86aa9de7681d48b078
aaronvelt <aaronvelt000@gmail.com>
2017-09-12 13:56:25 +0200

    H04 H05 H06: (alle opgaves)

--- src/h06/PraktijkH6.java
+++ src/h06/PraktijkH6.java
@@ -3,16 +3,14 @@ package h06;
 import java.applet.Applet;
 import java.awt.*;
 
-public class PositiefNegatief extends Applet {
-   private int a;
-
+public class PraktijkH6 extends Applet {
     @Override
     public void init() {
-        a = 2147483647;
+
     }
 
     @Override
     public void paint(Graphics g) {
-        g.drawString("hier is de uitkomst"+ (a+a),20,20);
+        g.drawString("Het gemmidelde is  "+(5.9+6.3+6.9)/3,20,20);
     }
 }
