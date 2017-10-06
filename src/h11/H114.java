f772e81161b37f68cd95fc742cecdae7cfa2b48f
aaronvelt <aaronvelt000@gmail.com>
2017-10-06 11:25:28 +0200

    testmap H04 H05 H06 H08 H10 H11: H111 H112 H113 H114

--- src/h11/H114.java
+++ src/h11/H114.java
@@ -1,6 +1,26 @@
 package h11;
 
 import java.applet.Applet;
+import java.awt.*;
 
 public class H114 extends Applet {
+    int  som;
+    int teller;
+    int drie;
+
+    @Override
+    public void init() {
+        teller = 0 ;
+        drie = 3;
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        int y = 20;
+        for (som = 0; som <30; som = drie * teller){
+            y += 20;
+            g.drawString(""+som,20,y);
+            teller++;
+        }
+    }
 }
